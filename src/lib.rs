88f79655fbcd775c213fbfe77b87ab9f68bee0c5
Rouven Himmelstein <r.himmelstein@tarent.de>
2023-07-24 15:03:11 +0200

    chore(lib): polishing

--- src/lib.rs
+++ src/lib.rs
@@ -3,24 +3,18 @@ use std::io::{BufRead, BufReader, Write};
 use std::process::{Command, Stdio};
 
 /// Represents a shell with root privileges.
-/// The shell is automatically closed when the struct is dropped.
-/// # Example
-/// ```
-/// use super_shell::RootShell;
-/// let mut root_shell = RootShell::new().expect("Failed to crate root shell");
-/// println!("{}", root_shell.execute("echo Hello $USER"));
-/// ```
 pub struct RootShell {
     shell_process: std::process::Child,
 }
 
-/// String that is appended to the end of each command, to indicate that the command has finished
+/// String that is appended to the end of each command, to indicate that the command has finished.
 const END_OF_COMMAND: &str = "~end-of-command~";
 
 /// Implementation of RootShell
 impl RootShell {
-    /// Creates a new root shell
-    /// Returns None if the root shell could not be created
+    /// Creates a new root shell.
+    /// Defaults to pkexec as super user provider and sh as shell.
+    /// Returns None if the root shell could not be created.
     /// # Example
     /// ```
     /// use super_shell::RootShell;
@@ -31,23 +25,24 @@ impl RootShell {
         Self::spawn_root_shell("pkexec", "sh")
     }
 
-    /// Creates a new root shell with the specified super user provider and shell
-    /// Returns None if the root shell could not be created
+    /// Creates a new root shell with the specified super user provider and shell.
+    /// Returns None if the root shell could not be created.
+    /// sudo as an interactive super user provider is currently not supported.
     /// # Parameter
     /// * `super_user_provider` - The command to use to get super user privileges
     /// * `shell` - The shell to use
     /// # Example
     /// ```
     /// use super_shell::RootShell;
-    /// let mut root_shell = RootShell::new_custom("sudo", "bash").expect("Failed to crate root shell");
+    /// let mut root_shell = RootShell::new_custom("gksu", "bash").expect("Failed to crate root shell");
     /// println!("{}", root_shell.execute("echo Hello $USER"));
     /// ```
     pub fn new_custom(super_user_provider: &str, shell: &str) -> Option<Self> {
         Self::spawn_root_shell(super_user_provider, shell)
     }
 
-    /// Creates a new root shell with the specified super user provider and shell
-    /// Returns None if the root shell could not be created
+    /// Creates a new root shell with the specified super user provider and shell.
+    /// Returns None if the root shell could not be created.
     /// # Parameter
     /// * `super_user_provider` - The command to use to get super user privileges
     /// * `shell` - The shell to use
@@ -71,8 +66,8 @@ impl RootShell {
         Some(root_shell)
     }
 
-    /// Executes a command in the root shell and returns the output trimmed
-    /// Blocks the current thread until the command is finished
+    /// Executes a command in the root shell and returns the output trimmed.
+    /// Blocks the current thread until the command is finished.
     /// # Parameter
     /// * `command` - The command to execute
     /// # Example
@@ -117,7 +112,7 @@ impl RootShell {
         cmd_response
     }
 
-    /// Exits the root shell and waits for the process to finish
+    /// Exits the root shell and waits for the process to finish.
     pub fn exit(&mut self) {
         let mut shell_stdin = self.shell_process.stdin.as_mut().unwrap();
         writeln!(&mut shell_stdin, "exit").unwrap();
@@ -126,9 +121,9 @@ impl RootShell {
     }
 }
 
-/// Drop implementation for RootShell
+/// Drop implementation for RootShell.
 impl Drop for RootShell {
-    /// Exits the root shell and waits for the process to finish
+    /// Exits the root shell and waits for the process to finish.
     fn drop(&mut self) {
         self.exit();
         self.shell_process.wait().expect("failed to wait on child");
@@ -140,7 +135,7 @@ mod tests {
     use crate::RootShell;
 
     #[test]
-    fn test_custom_sudo() {
+    fn test_custom() {
         let mut root_shell = RootShell::new_custom("sudo", "bash").unwrap();
         assert!(root_shell
             .execute("echo Hello $USER")
